9bbc7672be654f8a00dc231de340c5a4e0fddf6c
RedzCafe <software@devoredev.com>
2022-10-17 19:47:45 -0400

    sending email by Category

--- ContactPro/Models/ViewModels/EmailCategoryViewModel.cs
+++ ContactPro/Models/ViewModels/EmailCategoryViewModel.cs
@@ -3,7 +3,7 @@
     public class EmailCategoryViewModel
     {
 
-        public List<Contact> Contacts { get; set; }
+        public List<Contact>? Contacts { get; set; }
         public EmailData? EmailData { get; set; }
 
     }
