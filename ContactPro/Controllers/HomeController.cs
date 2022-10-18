1b6f48f43844f3d895a8ae0d0f859f7df9d9a948
RedzCafe <software@devoredev.com>
2022-10-18 10:52:20 -0400

    cleanup and qr code with authenticator

--- ContactPro/Controllers/HomeController.cs
+++ ContactPro/Controllers/HomeController.cs
@@ -23,6 +23,28 @@ namespace ContactPro.Controllers
             return View();
         }
 
+        [Route("/Home/HandleError/{code:int}")]
+        public IActionResult HandelError(int code)
+        {
+            var customError = new CustomError();
+            customError.code = code;
+
+            if (code == 404)
+            {
+                customError.message = "The Page you are looking for is not found";
+                // return View("~/Views/Shared/CustomError.cshtml",customError);
+                // you can create many cusom error pages for Specific Errors this way if you want, by creating a new HTML page per error
+            }
+            else
+            {
+                customError.message = "Sorry, there's been an error";
+            }
+            return View("~/Views/Shared/CustomError.cshtml",customError);
+
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -57,6 +57,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStatusCodePagesWithReExecute("/Home/HandleError/{0}");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
